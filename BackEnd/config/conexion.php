46e8670a8fac72e53e09ad321a6a933f41a9997e
Robin <robinjoel345@gmail.com>
2024-12-01 11:34:54 -0500

    Conexion a azure, archivo config

--- BackEnd/config/conexion.php
+++ BackEnd/config/conexion.php
@@ -1,13 +1,20 @@
 <?php
-$host = 'localhost';
-$dbname = 'procyfinal';
-$user = 'root';
-$password = '';
-
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    // Configuración de la conexión
+    $server = "tcp:desarrolloproyectos.database.windows.net,1433";
+    $database = "procyfinal";
+    $username = "desarrolladores";
+    $password = "m936x9soHJ8A962EW8x6";
+
+    // Crear la instancia de PDO
+    $pdo = new PDO("sqlsrv:server=$server;Database=$database", $username, $password);
+
+    // Configurar el modo de error de PDO
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //echo "Conexión exitosa a SQL Server usando PDO.";
 } catch (PDOException $e) {
-    die("Error en la conexión: " . $e->getMessage());
+    echo "Error de conexión: " . $e->getMessage();
 }
 ?>
+
